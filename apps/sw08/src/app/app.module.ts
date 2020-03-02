1d13b3d17ebe7c2babe0beeaaaf0de2a5faac46f
Steve Witman <stevewitman@gmail.com>
2020-03-02 07:33:02 -0700

    Added projects service to lib and added components: toolbar, projects, projects-list, project-details

--- apps/sw08/src/app/app.module.ts
+++ apps/sw08/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreDataModule } from '@bb/core-data';
 import { MaterialModule } from '@bb/material';
 import { UiToolbarModule } from '@bb/ui-toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ProjectsListComponent,
+    ProjectDetailsComponent,
   ],
   imports: [
     BrowserModule,
     CoreDataModule,
     MaterialModule,
     UiToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
